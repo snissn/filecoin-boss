26dfae35d6623c46bbde4e75383323ba17238184
Mikers <snissn@gmail.com>
2026-08-12 20:53:49 -1000

    test: define flat streaming lifecycle red gate

---
+++ test/unit/BossAccountFlatLifecycle.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {BossAccount} from "../../src/BossAccount.sol";
+import {BossTypes} from "../../src/libraries/BossTypes.sol";
+
+contract BossAccountFlatLifecycleRedTest {
+    function testFlatOfferLifecycleSurfaceExists() public {
+        BossAccount account = new BossAccount(
+            address(this),
+            address(0xF11E),
+            address(0x5100),
+            address(0xADA7),
+            1
+        );
+        BossTypes.AcceptanceInput memory input;
+
+        account.acceptOffer(input);
+    }
+}

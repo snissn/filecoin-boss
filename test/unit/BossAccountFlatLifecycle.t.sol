a25169833a8f729e790a6377ad66751736abbc38
Mikers <snissn@gmail.com>
2026-08-12 20:54:49 -1000

    style: format flat lifecycle red test

--- test/unit/BossAccountFlatLifecycle.t.sol
+++ test/unit/BossAccountFlatLifecycle.t.sol
@@ -6,13 +6,7 @@ import {BossTypes} from "../../src/libraries/BossTypes.sol";
 
 contract BossAccountFlatLifecycleRedTest {
     function testFlatOfferLifecycleSurfaceExists() public {
-        BossAccount account = new BossAccount(
-            address(this),
-            address(0xF11E),
-            address(0x5100),
-            address(0xADA7),
-            1
-        );
+        BossAccount account = new BossAccount(address(this), address(0xF11E), address(0x5100), address(0xADA7), 1);
         BossTypes.AcceptanceInput memory input;
 
         account.acceptOffer(input);
